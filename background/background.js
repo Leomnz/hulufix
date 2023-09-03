a6f70e520e1dccbde3519317a02cc6a3cecfcd65
John Stromberg <74280994+JohnStromberg@users.noreply.github.com>
2023-09-03 01:02:09 -0500

    Fixed permission requesting :)))))))

---
+++ background/background.js
@@ -0,0 +1,25 @@
+if (typeof browser == "undefined") {
+    // `browser` is not defined in Chrome, but Manifest V3 extensions in Chrome
+    // also support promises in the `chrome` namespace, like Firefox. To easily
+    // test the example without modifications, polyfill "browser" to "chrome".
+    globalThis.browser = chrome;
+}
+
+const background = {
+    // This origin is listed in host_permissions:
+    origins:["*://*.hulu.com/*"],
+};
+
+async function requestPermissions(){
+    await browser.permissions.request(background);
+    console.log(`Current permissions:`, background);
+};
+
+document.getElementById("permission_button").addEventListener("click", requestPermissions)
+
+browser.runtime.onInstalled.addListener(async () => {
+    browser.tabs.create({
+        url: browser.runtime.getURL("background/background.html")
+    });
+
+});
